b27b87635b194208dd8bbefbbb8c3e90e5579eca
baselkorj <baselanwarkorj@gmail.com>
2022-05-08 15:31:26 -0400

    dart project first commit

---
+++ bin/avr_disassembler.dart
@@ -0,0 +1,5 @@
+import 'package:avr_disassembler/avr_disassembler.dart' as avr_disassembler;
+
+void main(List<String> arguments) {
+  print('Hello world: ${avr_disassembler.calculate()}!');
+}
---
+++ test/avr_disassembler_test.dart
@@ -0,0 +1,8 @@
+import 'package:avr_disassembler/avr_disassembler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
