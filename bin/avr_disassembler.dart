3f0c00bf25cadf994deff7139781c760db83fbc2
baselkorj <baselanwarkorj@gmail.com>
2022-05-10 15:29:02 -0400

    fix HEX file iteration

--- bin/avr_disassembler.dart
+++ bin/avr_disassembler.dart
@@ -10,16 +10,24 @@ void main(List<String> arguments) {
 
   String tmp = File(arguments[0]).readAsStringSync();
 
-  int i = 0;
+  int i = 9;
 
   while (i < tmp.length) {
     int WORD = 0;
+    int j = 0;
+    double byteCount = int.parse(tmp[i - 8] + tmp[i - 7], radix: 16) / 2;
 
-    WORD = int.parse(tmp[i + 2] + tmp[i + 3] + tmp[i] + tmp[i + 1], radix: 16);
+    while (j < byteCount) {
+      WORD =
+          int.parse(tmp[i + 2] + tmp[i + 3] + tmp[i] + tmp[i + 1], radix: 16);
 
-    decoder.decodeInstruction(WORD);
+      decoder.decodeInstruction(WORD);
 
-    i += 4;
+      i += 4;
+      j++;
+    }
+
+    i += 13;
   }
 
   exit(0);
